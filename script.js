17389caa6be9d45c85a6212a84c80ea843bc2c7d
oswaldinho23 <60334049+oswaldinho23@users.noreply.github.com>
2024-09-24 00:11:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function guardarInfo() {
+    const objetivo = document.getElementById('objetivo').value;
+    const experiencia = document.getElementById('experiencia').value;
+    const estudios = document.getElementById('estudios').value;
+
+    alert(`Información Guardada:\n\nObjetivo: ${objetivo}\nExperiencia: ${experiencia}\nEstudios: ${estudios}`);
+}
